a47261f53fb5b2a10fef8729fff822b056d56af9
mjtiss <up202106867@up.pt>
2025-04-14 23:01:19 +0100

    Add files via upload

---
+++ game.py
@@ -0,0 +1,49 @@
+from ConnectsState import ConnectState
+from mcts import MCTS 
+from meta import GameMeta
+from meta import MCTSMeta
+
+
+
+#game.py -> main class to play with agent
+def play():
+    state = ConnectState()
+    mcts = MCTS(state)
+
+    while not state.game_over():
+        print("Current state:")
+        state.print()
+
+        user_move = int(input("Enter a move: "))
+        while user_move not in state.get_legal_moves():
+            print("Illegal move")
+            user_move = int(input("Enter a move: "))
+
+        state.move(user_move)
+        mcts.move(user_move)
+
+        state.print()
+
+        if state.game_over():
+            print("Player one won!")
+            break
+
+        print("Thinking...")
+
+        mcts.search(max_iterations = 100000, time_limit = 2.0)
+        num_rollouts, run_time = mcts.statistics()
+        print("Statistics: ", num_rollouts, "rollouts in", run_time, "seconds")
+        move = mcts.best_move()
+
+        print("MCTS chose move: ", move)
+
+        state.move(move)
+        mcts.move(move)
+
+        if state.game_over():
+            print("Player two won!")
+            break
+
+
+if __name__ == "__main__":
+    play()
